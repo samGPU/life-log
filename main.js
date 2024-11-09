9f4720c82b9f5c7109e8adbafce7ce8b844600bc
Sam Buckley <samgpu@proton.me>
2024-11-09 12:52:40 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Life Log</h1>
+    <p>
+      Soon to be an app to help me track my life
+    </p>
+  </div>
+`
